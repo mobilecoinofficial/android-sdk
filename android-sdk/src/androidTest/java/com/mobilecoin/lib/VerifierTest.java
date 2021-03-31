8878effc58998637b418211e32e2e125914e6bfc
Sam <sam.dealy@gmail.com>
2021-03-31 15:37:23 -0700

    Create unit tests for Verifier class

--- android-sdk/src/androidTest/java/com/mobilecoin/lib/VerifierTest.java
+++ android-sdk/src/androidTest/java/com/mobilecoin/lib/VerifierTest.java
@@ -14,12 +14,13 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class VerifierTest {
 
-    @Rule public ExpectedException exceptionChecker = ExpectedException.none();
+    @Rule
+    public ExpectedException exceptionChecker = ExpectedException.none();
 
     private static final short PRODUCT_ID = 3;
     private static final short SECURITY_VERSION = 1;
-    private static final String[] CONFIG_ADVISORIES = { "INTEL-SA-00391" };
-    private static final String[] HARDENING_ADVISORIES = { "INTEL-SA-00334" };
+    private static final String[] CONFIG_ADVISORIES = {"INTEL-SA-00391"};
+    private static final String[] HARDENING_ADVISORIES = {"INTEL-SA-00334"};
 
     private Verifier verifier;
 
@@ -70,9 +71,9 @@ public class VerifierTest {
     @Test
     public void withMrSigner_validMrSigner_doesNotThrowException() throws Exception {
         // Corresponds to 32 unsigned 8 bit integers.
-        String VALID_MR_SIGNER_HEX =
+        String validMrSigner =
                 "7ee5e29d74623fdbc6fbf1454be6f3bb0b86c12366b7b478ad13353e44de8411";
-        verifier.withMrSigner(Hex.toByteArray(VALID_MR_SIGNER_HEX), PRODUCT_ID, SECURITY_VERSION,
+        verifier.withMrSigner(Hex.toByteArray(validMrSigner), PRODUCT_ID, SECURITY_VERSION,
                 CONFIG_ADVISORIES, HARDENING_ADVISORIES);
     }
 
@@ -93,7 +94,7 @@ public class VerifierTest {
     }
 
     @Test
-    public void withMrEnclave_mrSignerHasLessThan32Digits_throwsAttestationException() throws Exception {
+    public void withMrEnclave_mrEnclaveHasLessThan32Digits_throwsAttestationException() throws Exception {
         // Corresponds to 9 unsigned 8 bit integers.
         String lessThan32DigitsMrEnclaveHex = "7ee5e29d74623fdb1f";
 
@@ -103,24 +104,23 @@ public class VerifierTest {
     }
 
     @Test
-    public void withMrEnclave_mrSignerHasMoreThan32Digits_throwsAttestationException() throws Exception {
+    public void withMrEnclave_mrEnclaveHasMoreThan32Digits_throwsAttestationException() throws Exception {
         // Corresponds to 33 unsigned 8 bit integers.
-        String moreThan32DigitsMrSignerHex =
+        String moreThan32DigitsMrEnclaveHex =
                 "7ee5e29d74623fdbc6fbf1454be6f3bb0b86c12366b7b478ad13353e44de8411ff";
 
         exceptionChecker.expect(AttestationException.class);
-        verifier.withMrEnclave(Hex.toByteArray(moreThan32DigitsMrSignerHex),
+        verifier.withMrEnclave(Hex.toByteArray(moreThan32DigitsMrEnclaveHex),
                 CONFIG_ADVISORIES, HARDENING_ADVISORIES);
     }
 
     @Test
     public void withMrEnclave_validMrEnclave_doesNotThrowException() throws Exception {
         // Corresponds to 32 unsigned 8 bit integers.
-        String VALID_MR_ENCLAVE_HEX =
+        String validMrEnclaveHex =
                 "7ee5e29d74623fdbc6fbf1454be6f3bb0b86c12366b7b478ad13353e44de8411";
 
-        exceptionChecker.expect(AttestationException.class);
-        verifier.withMrEnclave(Hex.toByteArray(VALID_MR_ENCLAVE_HEX),
+        verifier.withMrEnclave(Hex.toByteArray(validMrEnclaveHex),
                 CONFIG_ADVISORIES, HARDENING_ADVISORIES);
     }
 }
