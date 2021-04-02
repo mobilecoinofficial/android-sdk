d0c3d12eb674c75b79d00f38577a40facaeeb529
Sam <sam.dealy@gmail.com>
2021-04-02 11:39:33 -0700

    Make minor readability improvements to UTXOSelector

--- android-sdk/src/main/java/com/mobilecoin/lib/UTXOSelector.java
+++ android-sdk/src/main/java/com/mobilecoin/lib/UTXOSelector.java
@@ -4,6 +4,7 @@ package com.mobilecoin.lib;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import com.mobilecoin.lib.exceptions.FragmentedAccountException;
 import com.mobilecoin.lib.exceptions.InsufficientFundsException;
@@ -17,16 +18,19 @@ import java.util.Vector;
 import java.util.stream.Collectors;
 
 /**
- * Provides the TXO selection for the AccountKey defragmentation, fee estimations, and transaction
- * composition purposes.
+ * Utility methods that provide the Transaction Output (TXO) selection for the {@link AccountKey}
+ * defragmentation, fee estimations, and transaction composition.
  */
-class UTXOSelector {
-    private static final String TAG = UTXOSelector.class.getName();
+final class UTXOSelector {
+
     public static final int MAX_INPUTS = 16;
+    private static final String TAG = UTXOSelector.class.getName();
+
+    private UTXOSelector() {}
 
     /**
-     * Select optimal TxOuts for merging during the account de-fragmentation This method will select
-     * up to MAX_INPUTS elements
+     * Selects the optimal TxOuts for merging during account de-fragmentation. This method will
+     * select up to MAX_INPUTS elements.
      *
      * @param inputs    a set of unspent OwnedTxOuts to select from
      * @param txFee     the fee amount to post a transaction
@@ -57,6 +61,7 @@ class UTXOSelector {
     }
 
     @NonNull
+    @VisibleForTesting
     static TxOutNode selectTxOutNodesForMerging(
             @NonNull List<TxOutNode> inputs,
             @NonNull BigInteger txFee,
@@ -64,9 +69,9 @@ class UTXOSelector {
             @NonNull BigInteger outputFee
     ) throws InsufficientFundsException {
         Logger.i(TAG, "Selecting TxOutNodes for merging");
-        // sort in descending order so it's easy to pick up the largest TxOuts first
+        // Sort in descending order so it's easy to pick up the largest TxOuts first.
         List<TxOutNode> txOuts = inputs.stream()
-                .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+                .sorted((firstNode, secondNode) -> secondNode.getValue().compareTo(firstNode.getValue()))
                 .collect(Collectors.toList());
 
         BigInteger totalAmountAvailable = inputs.stream()
@@ -74,11 +79,10 @@ class UTXOSelector {
                 .reduce(BigInteger.ZERO, BigInteger::add);
 
         List<TxOutNode> result = new ArrayList<>();
-        // initial fee is tx fee + one output fee
         BigInteger fee = txFee.add(outputFee);
         BigInteger selectionAmount = BigInteger.ZERO;
-        // select up to MAX_INPUTS txOut nodes
         for (TxOutNode txOutNode : txOuts) {
+            // select up to MAX_INPUTS txOut nodes
             if (result.size() == MAX_INPUTS) break;
             BigInteger newSelectionAmount = selectionAmount.add(txOutNode.getValue());
             BigInteger newFee = fee.add(txOutNode.getFee(txFee, inputFee));
@@ -95,7 +99,7 @@ class UTXOSelector {
     }
 
     /**
-     * Select a minimal number of UTXOs required to cover the specified amount. The selection
+     * Selects a minimal number of UTXOs required to cover the specified amount. The selection
      * algorithm is looking for the largest UTXO that can cover the amount plus the smallest UTXO to
      * reduce the account fragmentation. If such UTXO cannot be found, add the largest available
      * UTXO and repeat the selection again for the remainder.
@@ -120,10 +124,10 @@ class UTXOSelector {
             int outputsCount
     ) throws InsufficientFundsException, FragmentedAccountException {
         Logger.i(TAG, "Selecting inputs for amount", null,
-                      "amount:", amount,
-                      "txFee:", txFee,
-                      "inputFee:", inputFee,
-                      "outputFee:", outputFee);
+                "amount:", amount,
+                "txFee:", txFee,
+                "inputFee:", inputFee,
+                "outputFee:", outputFee);
         List<TxOutNode> nodes = inputs.stream()
                 .map(ownedTxOut -> new TxOutNode(ownedTxOut, null))
                 .collect(Collectors.toList());
@@ -143,6 +147,7 @@ class UTXOSelector {
     }
 
     @NonNull
+    @VisibleForTesting
     static Selection<TxOutNode> selectTxOutNodesForAmount(
             @NonNull List<TxOutNode> inputs,
             @NonNull BigInteger amount,
@@ -152,11 +157,11 @@ class UTXOSelector {
             int outputsCount
     ) throws InsufficientFundsException, FragmentedAccountException {
         Logger.i(TAG, "Selecting TxOutNodes for amount", null,
-                      "amount:", amount,
-                      "txFee:", txFee,
-                      "inputFee:", inputFee,
-                      "outputFee:", outputFee);
-        BigInteger totalAvailable = inputs.stream()
+                "amount:", amount,
+                "txFee:", txFee,
+                "inputFee:", inputFee,
+                "outputFee:", outputFee);
+        BigInteger totalMobAvailable = inputs.stream()
                 .map(TxOutNode::getValue)
                 .reduce(BigInteger.ZERO, BigInteger::add);
 
@@ -164,47 +169,47 @@ class UTXOSelector {
             throw new InsufficientFundsException();
         }
 
-        // sort vector in the ascending order so it's easy to pick up smalled/largest TxOuts
+        // Sort the vector in the ascending order so it's easy to pick up smalled/largest TxOuts.
         Vector<TxOutNode> sortedInputsVector = inputs.stream()
-                .sorted((a, b) -> a.getValue().compareTo(b.getValue()))
+                .sorted((firstNode, secondNode) -> firstNode.getValue().compareTo(secondNode.getValue()))
                 .collect(Collectors.toCollection(Vector::new));
 
-        BigInteger fee = txFee.add(BigInteger.valueOf(outputsCount).multiply(outputFee));
-        List<TxOutNode> selected = new ArrayList<>();
-        // add the first smallest input and the input fee
+        BigInteger totalFee = txFee.add(BigInteger.valueOf(outputsCount).multiply(outputFee));
+        List<TxOutNode> selectedTxOutNodes = new ArrayList<>();
+        // Add the first smallest input and the input fee.
         TxOutNode input = sortedInputsVector.firstElement();
-        fee = fee.add(input.getFee(txFee, inputFee));
-        selected.add(input);
-        // remove the input from available inputs
+        totalFee = totalFee.add(input.getFee(txFee, inputFee));
+        selectedTxOutNodes.add(input);
+        // Remove the input from available inputs.
         sortedInputsVector.removeElement(input);
 
         BigInteger selectedAmount = input.getValue();
-        while (sortedInputsVector.size() > 0 && selected.size() < MAX_INPUTS) {
-            if (selectedAmount.compareTo(amount.add(fee)) >= 0) {
+        while (sortedInputsVector.size() > 0 && selectedTxOutNodes.size() < MAX_INPUTS) {
+            if (selectedAmount.compareTo(amount.add(totalFee)) >= 0) {
                 break;
             }
-            // add largest value TxOut and update the fee
+            // Add largest value TxOut and update the fee.
             input = sortedInputsVector.lastElement();
-            selected.add(input);
-            fee = fee.add(input.getFee(txFee, inputFee));
+            selectedTxOutNodes.add(input);
+            totalFee = totalFee.add(input.getFee(txFee, inputFee));
             sortedInputsVector.removeElement(input);
             selectedAmount = selectedAmount.add(input.getValue());
         }
 
-        if (totalAvailable.compareTo(amount.add(fee)) < 0) {
+        if (totalMobAvailable.compareTo(amount.add(totalFee)) < 0) {
             throw new InsufficientFundsException();
         }
 
-        if (selectedAmount.compareTo(amount.add(fee)) < 0) {
+        if (selectedAmount.compareTo(amount.add(totalFee)) < 0) {
             throw new FragmentedAccountException(
                     "The account requires defragmentation to send the required amount");
         }
 
-        return new Selection<>(selected, fee);
+        return new Selection<>(selectedTxOutNodes, totalFee);
     }
 
     /**
-     * Calculate the total fee required to send a specified amount from the list of unspent inputs
+     * Calculates the total fee required to send a specified amount from the list of unspent inputs.
      *
      * @param unspent      a list of unspent TxOuts to select from
      * @param amount       the amount to calculate fee for
@@ -223,10 +228,10 @@ class UTXOSelector {
             int outputsCount
     ) throws InsufficientFundsException {
         Logger.i(TAG, "Calculating fee", null,
-                      "amount:", amount,
-                      "txFee:", txFee,
-                      "inputFee:", inputFee,
-                      "outputFee:", outputFee);
+                "amount:", amount,
+                "txFee:", txFee,
+                "inputFee:", inputFee,
+                "outputFee:", outputFee);
         // convert inputs into promises for simplify calculation
         List<TxOutNode> inputs = unspent.stream().map(
                 txOut -> new TxOutNode(txOut, null)
@@ -262,14 +267,13 @@ class UTXOSelector {
     }
 
     /**
-     * Calculate the total transferable amount excluding all the required fees for such transfer
+     * Calculate the total transferable amount excluding all the required fees for such transfer.
      *
      * @param unspent   a list of unspent TxOuts to select from
      * @param txFee     the fee amount to post a transaction
      * @param inputFee  the fee per each transaction input
      * @param outputFee the fee per each transaction output
      */
-
     public static BigInteger getTransferableAmount(
             @NonNull Set<OwnedTxOut> unspent,
             @NonNull BigInteger txFee,
@@ -277,11 +281,11 @@ class UTXOSelector {
             @NonNull BigInteger outputFee
     ) throws InsufficientFundsException {
         Logger.i(TAG, "Getting transferable amount", null,
-                      "unspent:", unspent,
-                      "txFee:", txFee,
-                      "inputFee:", inputFee,
-                      "outputFee:", outputFee);
-        // convert inputs into promises for simplify calculation
+                "unspent:", unspent,
+                "txFee:", txFee,
+                "inputFee:", inputFee,
+                "outputFee:", outputFee);
+        // Convert inputs into promises to simplify calculation.
         List<TxOutNode> inputs = unspent.stream()
                 .filter(txOut -> txOut.getValue().compareTo(inputFee) > 0)
                 .map(txOut -> new TxOutNode(txOut, null)
@@ -307,6 +311,7 @@ class UTXOSelector {
         return totalAmountAvailable.subtract(fees);
     }
 
+    /** Represents a selection of transaction outputs. */
     static class Selection<T> {
         public final List<T> txOuts;
         public final BigInteger fee;
@@ -320,6 +325,12 @@ class UTXOSelector {
         }
     }
 
+    /**
+     *  Represents a transaction output.
+     *
+     *  <p>Can contain "children", which are other transaction outputs that were merged into this
+     *  "parent" transaction output.
+     */
     static class TxOutNode {
         public final OwnedTxOut txOut;
         public final List<TxOutNode> children;
