d38250b20046796c07e4de5fdd8761fd9f6e26a7
Sam Dealy <sam.dealy@gmail.com>
2021-08-27 13:06:27 -0400

    Create SenderMemo class

---
+++ android-sdk/src/main/java/com/mobilecoin/lib/SenderMemo.java
@@ -0,0 +1,95 @@
+package com.mobilecoin.lib;
+
+import androidx.annotation.NonNull;
+import com.mobilecoin.lib.exceptions.InvalidTxOutMemoException;
+
+/** Represents a sender memo, which corresponds to the "AuthenticatedSenderMemo" specificatoin. */
+public final class SenderMemo extends Native implements TxOutMemo {
+
+  private static final String TAG = SenderMemo.class.getSimpleName();
+
+  private SenderMemoData senderMemoData;
+
+  /**
+   * Creates a {@link SenderMemo} from a decrypted memo data that hasn't been validated yet.
+   *
+   * @param memoData - The 44 bytes that correspond to the memo payload.
+   **/
+  static SenderMemo create(@NonNull byte[] memoData) {
+    if (memoData.length != 44) {
+      throw new IllegalArgumentException("Memo data byte array must have a lenght of 44. Instead, the length was: " + memoData.length);
+    }
+    return new SenderMemo(memoData);
+  }
+
+  @Override
+  public TxOutMemoType getTxOutMemoType() {
+    return TxOutMemoType.SENDER;
+  }
+
+  private SenderMemo(byte[] memoData) {
+    try {
+      init_jni_from_memo_data(memoData);
+    } catch(Exception e) {
+      IllegalArgumentException illegalArgumentException =
+          new IllegalArgumentException("Failed to create an AccountKey", e);
+      Util.logException(TAG, illegalArgumentException);
+      throw illegalArgumentException;
+    }
+  }
+
+
+  /**
+   * Retrieves the {@link AddressHash} that hasn't been validated yet.
+   *
+   * <p>This is used to see if the user who wrote the sender memo is known by the current user.
+   * */
+  public AddressHash getUnvalidatedAddressHash() {
+    return getAddressHash();
+  }
+
+  private AddressHash getAddressHash() {
+    byte[] addressHashData = get_address_hash_data();
+    return AddressHash.createAddressHash(addressHashData);
+  }
+
+  /**
+   * Validates then retrieves the sender memo data.
+   *
+   * <p>Before calling this method, call {@link #getUnvalidatedAddressHash()} and see if the
+   * {@link AddressHash} corresponds to a {@link PublicAddress} that is known by the user.
+   * If the {@link PublicAddress} is not known by the user, then do not call this method- the
+   * memo is automatically invalid.
+   **/
+  public SenderMemoData getSenderMemoData(
+      @NonNull PublicAddress senderPublicAddress,
+      @NonNull RistrettoPrivate receivingSubaddressViewPrivateKey,
+      @NonNull RistrettoPublic txOutPublicKey
+  ) throws InvalidTxOutMemoException {
+    if (senderMemoData != null) {
+      return senderMemoData;
+    }
+
+    if (!is_valid(
+        senderPublicAddress,
+        receivingSubaddressViewPrivateKey,
+        txOutPublicKey
+    )) {
+      throw new InvalidTxOutMemoException("The sender memo is invalid.");
+    }
+
+    senderMemoData = SenderMemoData.create(getAddressHash());
+    return senderMemoData;
+  }
+
+  private native void init_jni_from_memo_data(byte[] memoData);
+
+  // Returns true if the sender memo is valid.
+  private native boolean is_valid(
+      @NonNull PublicAddress senderPublicAddress,
+      @NonNull RistrettoPrivate receivingSubaddressViewPrivateKey,
+      @NonNull  RistrettoPublic txOutPublicKey
+  );
+
+  private native byte[] get_address_hash_data();
+}
