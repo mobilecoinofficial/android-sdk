8c7401fdf293bd4cad782f70d59e63224b9b7f95
Alex Voloshyn <alexv@mobilecoin.com>
2021-12-03 17:50:02 -0800

    add bip39 entropy support to TransferPayload (#55)

--- android-sdk/src/main/java/com/mobilecoin/lib/TransferPayload.java
+++ android-sdk/src/main/java/com/mobilecoin/lib/TransferPayload.java
@@ -3,6 +3,7 @@
 package com.mobilecoin.lib;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.google.protobuf.ByteString;
 import com.mobilecoin.api.Printable;
@@ -15,33 +16,59 @@ import java.util.Objects;
 public class TransferPayload {
     private final static String TAG = TransferPayload.class.getName();
     private final byte[] rootEntropy;
+    private final byte[] bip39Entropy;
     private final RistrettoPublic publicKey;
     private final String memo;
 
-    public TransferPayload(
-            @NonNull byte[] rootEntropy,
+    private TransferPayload(
+            @Nullable byte[] rootEntropy,
+            @Nullable byte[] bip39Entropy,
             @NonNull RistrettoPublic publicKey,
             @NonNull String memo
     ) {
         this.rootEntropy = rootEntropy;
+        this.bip39Entropy = bip39Entropy;
         this.publicKey = publicKey;
         this.memo = memo;
+    }
+    public static TransferPayload fromRootEntropy(
+            @NonNull byte[] rootEntropy,
+            @NonNull RistrettoPublic publicKey,
+            @NonNull String memo
+    ) {
+        return new TransferPayload(rootEntropy, null, publicKey, memo);
+    }
+
+    public static TransferPayload fromBip39Entropy(
+            @NonNull byte[] bip39Entropy,
+            @NonNull RistrettoPublic publicKey,
+            @NonNull String memo
+    ) {
+        return new TransferPayload(null, bip39Entropy, publicKey, memo);
     }
 
     @NonNull
     static TransferPayload fromProtoBufObject(@NonNull Printable.TransferPayload protoBuf)
             throws SerializationException {
         Logger.i(TAG, "Initializing from protobuf");
+        ByteString rootEntropyString = protoBuf.getRootEntropy();
+        ByteString bip39EntropyString = protoBuf.getBip39Entropy();
         return new TransferPayload(
-                protoBuf.getBip39Entropy().toByteArray(),
+                rootEntropyString != null ? rootEntropyString.toByteArray() : null,
+                bip39EntropyString != null ? bip39EntropyString.toByteArray() : null,
                 RistrettoPublic.fromBytes(protoBuf.getTxOutPublicKey().getData().toByteArray()),
                 protoBuf.getMemo()
         );
     }
 
-    @NonNull
+    @Nullable
     public byte[] getRootEntropy() {
         return rootEntropy;
+    }
+
+    @Nullable
+    public byte[] getBip39Entropy() {
+        return bip39Entropy;
     }
 
     @NonNull
@@ -59,9 +86,17 @@ public class TransferPayload {
     @NonNull
     Printable.TransferPayload toProtoBufObject() {
         Logger.i(TAG, "Serializing to protobuf");
-        return Printable.TransferPayload.newBuilder().setMemo(getMemo())
-                .setTxOutPublicKey(getPublicKey().toProtoBufObject())
-                .setBip39Entropy(ByteString.copyFrom(rootEntropy)).build();
+        Printable.TransferPayload.Builder payloadBuilder = Printable.TransferPayload.newBuilder();
+        payloadBuilder
+                .setMemo(getMemo())
+                .setTxOutPublicKey(getPublicKey().toProtoBufObject());
+        if (rootEntropy != null) {
+           payloadBuilder.setRootEntropy(ByteString.copyFrom(rootEntropy));
+        }
+        if (bip39Entropy != null) {
+            payloadBuilder.setBip39Entropy(ByteString.copyFrom(bip39Entropy));
+        }
+        return payloadBuilder.build();
     }
 
     @Override
