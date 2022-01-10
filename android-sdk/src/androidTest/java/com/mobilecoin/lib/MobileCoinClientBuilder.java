e6d5e0f9613d46e182c5a8e6b3cf0cb272985322
Bernie Dolan <bernie@mobilecoin.com>
2022-01-10 11:25:58 -0800

    Test Short Fog URI (#58)
    * Test Short Fog URI
    
    Add test for short for uri
    Fixed an issue where MobileCoinClientBuilder would only take some values from the TestFogConfig after being set manually
    
    * Removing Short Fog URI from TestFogConfig

--- android-sdk/src/androidTest/java/com/mobilecoin/lib/MobileCoinClientBuilder.java
+++ android-sdk/src/androidTest/java/com/mobilecoin/lib/MobileCoinClientBuilder.java
@@ -1,39 +1,86 @@
 package com.mobilecoin.lib;
 
+import android.net.Uri;
+
 import com.mobilecoin.lib.exceptions.InvalidUriException;
 import com.mobilecoin.lib.network.TransportProtocol;
+
+import java.util.List;
 
 /** Builds a {@link MobileCoinClient} using supplied arguments. */
 public final class MobileCoinClientBuilder {
 
   private AccountKey accountKey;
-  private TestFogConfig testFogConfig;
+  private Uri fogUri;
+  private List<Uri> consensueUris;
+  private ClientConfig clientConfig;
+  private String username;
+  private String password;
   private TransportProtocol transportProtocol;
 
   /** Returns a {@link MobileCoinClientBuilder} with sensible defaults. */
   public static MobileCoinClientBuilder newBuilder() {
     TestFogConfig fogConfig = TestFogConfig.getFogConfig(Environment.CURRENT_TEST_ENV);
-    return new MobileCoinClientBuilder(
-            TestKeysManager.getNextAccountKey(),
-            fogConfig,
-            fogConfig.getTransportProtocol());
+    return new MobileCoinClientBuilder(fogConfig);
+  }
+
+  private MobileCoinClientBuilder(TestFogConfig testFogConfig) {
+    this(TestKeysManager.getNextAccountKey(),
+            testFogConfig.getFogUri(),
+            testFogConfig.getConsensusUris(),
+            testFogConfig.getClientConfig(),
+            testFogConfig.getUsername(),
+            testFogConfig.getPassword(),
+            testFogConfig.getTransportProtocol());
   }
 
   private MobileCoinClientBuilder(AccountKey accountKey,
-                                  TestFogConfig testFogConfig,
+                                  Uri fogUri,
+                                  List<Uri> consensusUris,
+                                  ClientConfig clientConfig,
+                                  String username,
+                                  String password,
                                   TransportProtocol transportProtocol) {
     this.accountKey = accountKey;
-    this.testFogConfig = testFogConfig;
+    this.fogUri = fogUri;
+    this.consensueUris = consensusUris;
+    this.clientConfig = clientConfig;
+    this.username = username;
+    this.password = password;
     this.transportProtocol = transportProtocol;
   }
 
   public MobileCoinClientBuilder setTestFogConfig(TestFogConfig testFogConfig) {
-    this.testFogConfig = testFogConfig;
-    return this;
+    return new MobileCoinClientBuilder(testFogConfig);
   }
 
   public MobileCoinClientBuilder setAccountKey(AccountKey accountKey) {
     this.accountKey = accountKey;
+    return this;
+  }
+
+  public MobileCoinClientBuilder setFogUri(Uri fogUri) {
+    this.fogUri = fogUri;
+    return this;
+  }
+
+  public MobileCoinClientBuilder setConsensueUris(List<Uri> consensueUris) {
+    this.consensueUris = consensueUris;
+    return this;
+  }
+
+  public MobileCoinClientBuilder setClientConfig(ClientConfig clientConfig) {
+    this.clientConfig = clientConfig;
+    return this;
+  }
+
+  public MobileCoinClientBuilder setUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public MobileCoinClientBuilder setPassword(String password) {
+    this.password = password;
     return this;
   }
 
@@ -45,21 +92,50 @@ public final class MobileCoinClientBuilder {
   /** Constructs a {@link MobileCoinClient} based on the supplied arguments. */
   public MobileCoinClient build() throws InvalidUriException {
     MobileCoinClient mobileCoinClient = new MobileCoinClient(
-        accountKey,
-        testFogConfig.getFogUri(),
-        testFogConfig.getConsensusUris(),
-        testFogConfig.getClientConfig(),
-        testFogConfig.getTransportProtocol()
+        this.accountKey,
+        this.fogUri,
+        this.consensueUris,
+        this.clientConfig,
+        this.transportProtocol
     );
     mobileCoinClient.setFogBasicAuthorization(
-        testFogConfig.getUsername(),
-        testFogConfig.getPassword()
+        this.username,
+        this.password
     );
     mobileCoinClient.setConsensusBasicAuthorization(
-        testFogConfig.getUsername(),
-        testFogConfig.getPassword()
+        this.username,
+        this.password
     );
 
     return mobileCoinClient;
   }
+
+  public AccountKey getAccountKey() {
+    return this.accountKey;
+  }
+
+  public Uri getFogUri() {
+    return this.fogUri;
+  }
+
+  public List<Uri> getConsensueUris() {
+    return this.consensueUris;
+  }
+
+  public ClientConfig getClientConfig() {
+    return this.clientConfig;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public TransportProtocol getTransportProtocol() {
+    return this.transportProtocol;
+  }
+
 }
