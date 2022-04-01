ddaabaa4b1dfb525fae80434cf412b83074895b5
Bernie Dolan <bernie@mobilecoin.com>
2022-04-01 15:57:35 -0400

    Adding Javadoc to FogSyncException

--- android-sdk/src/main/java/com/mobilecoin/lib/exceptions/FogSyncException.java
+++ android-sdk/src/main/java/com/mobilecoin/lib/exceptions/FogSyncException.java
@@ -5,6 +5,12 @@ import androidx.annotation.Nullable;
 
 import com.mobilecoin.lib.UnsignedLong;
 
+/**
+ * This Exception is thrown when Fog is in an invalid state
+ *
+ * Fog View and Fog Ledger each have their own index indicating their highest processed block count.
+ * This Exception is thrown if these values are out of sync by 10 blocks or if Fog is out of sync with Consensus by 10 blocks .
+ */
 public class FogSyncException extends MobileCoinException {
 
     public FogSyncException(@NonNull Throwable throwable) {
