ab2431f57b22071f2fd8aafa18b2e5e1be81e471
Bernie Dolan <bernie@mobilecoin.com>
2022-05-09 22:46:18 -0700

    Parcelable implementation for Amount

--- android-sdk/src/main/java/com/mobilecoin/lib/Amount.java
+++ android-sdk/src/main/java/com/mobilecoin/lib/Amount.java
@@ -1,10 +1,13 @@
 package com.mobilecoin.lib;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 
 import java.math.BigInteger;
 
-public class Amount {
+public class Amount implements Parcelable {
 
     private final BigInteger value;
     private final UnsignedLong tokenId;
@@ -23,5 +26,33 @@ public class Amount {
     public UnsignedLong getTokenId() {
         return this.tokenId;
     }
+
+    private Amount(@NonNull Parcel parcel) {
+        value = (BigInteger)parcel.readSerializable();
+        tokenId = parcel.readParcelable(UnsignedLong.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int flags) {
+        parcel.writeSerializable(value);
+        parcel.writeParcelable(tokenId, flags);
+    }
+
+    public static final Creator<Amount> CREATOR = new Creator<Amount>() {
+        @Override
+        public Amount createFromParcel(@NonNull Parcel parcel) {
+            return new Amount(parcel);
+        }
+
+        @Override
+        public Amount[] newArray(int length) {
+            return new Amount[length];
+        }
+    };
 
 }
