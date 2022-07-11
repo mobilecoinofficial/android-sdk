1c091632f173ae1495748a72f8eedcd7011b118a
Bernie Dolan <bernie@mobilecoin.com>
2022-07-11 10:50:30 -0700

    Add ProposeTxResult to InvalidTransactionException

--- android-sdk/src/main/java/com/mobilecoin/lib/exceptions/InvalidTransactionException.java
+++ android-sdk/src/main/java/com/mobilecoin/lib/exceptions/InvalidTransactionException.java
@@ -2,10 +2,30 @@
 
 package com.mobilecoin.lib.exceptions;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import consensus_common.ConsensusCommon;
+
 public final class InvalidTransactionException extends MobileCoinException {
+
+    @Nullable
+    private final ConsensusCommon.ProposeTxResult result;
+
+    public InvalidTransactionException(@NonNull ConsensusCommon.ProposeTxResult result) {
+        super(result.toString());
+        this.result = result;
+    }
+
+    @Deprecated
     public InvalidTransactionException(@Nullable String message) {
         super(message);
+        this.result = null;
     }
+
+    @Nullable
+    public ConsensusCommon.ProposeTxResult getResult() {
+        return this.result;
+    }
+
 }
