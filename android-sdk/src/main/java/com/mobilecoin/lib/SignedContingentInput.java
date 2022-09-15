c426c737c9602ef4c86469999523ea294f2d839c
Bernie Dolan <bernie@mobilecoin.com>
2022-09-15 00:47:54 -0700

    Altering how SCI income and outlay are presented

--- android-sdk/src/main/java/com/mobilecoin/lib/SignedContingentInput.java
+++ android-sdk/src/main/java/com/mobilecoin/lib/SignedContingentInput.java
@@ -38,24 +38,55 @@ public class SignedContingentInput extends Native {
         Amount changeDeduction = Arrays.stream(getRequiredOutputAmounts())
                 .filter(amount -> grossIncome.getTokenId().equals(amount.getTokenId()))
                 .reduce(Amount::add).get();
+        if(changeDeduction.compareTo(grossIncome) >= 0) {
+            /*
+            If the "change" amount is more than the income, there is no income.
+            Instead of returning a negative income, the (income - change) will appear as a positive
+            outlay in {@link SignedContingentInput#getTotalOutlays}
+            */
+            return new Amount(BigInteger.ZERO, grossIncome.getTokenId());
+        }
         return grossIncome.subtract(changeDeduction);
     }
 
     @NonNull
     public Map<TokenId, Amount> getTotalOutlays() {
+
         final Amount[] requiredAmounts = getRequiredOutputAmounts();
         Map<TokenId, Amount> totalOutlays = new HashMap<>();
-        for(Amount amount : requiredAmounts) {
-            //TODO: on API level 24, we can use getOrDefault to simplify the logic here
-            Amount existingAmount = totalOutlays.get(amount.getTokenId());
-            if(null == existingAmount) existingAmount = new Amount(BigInteger.ZERO, amount.getTokenId());
-            totalOutlays.put(amount.getTokenId(), existingAmount.add(amount));
-        }
+
         final Amount pseudoOutputAmount = getPseudoOutputAmount();
-        Amount changeAdjustment = totalOutlays.get(pseudoOutputAmount.getTokenId());
-        if(null == changeAdjustment) changeAdjustment = new Amount(BigInteger.ZERO, pseudoOutputAmount.getTokenId());
-        totalOutlays.put(pseudoOutputAmount.getTokenId(), pseudoOutputAmount.subtract(changeAdjustment));
+        Amount changeAmount = new Amount(BigInteger.ZERO, pseudoOutputAmount.getTokenId());
+
+        for(Amount amount : requiredAmounts) {
+            if(changeAmount.getTokenId().equals(amount.getTokenId())) {
+                /*
+                If there are any required amounts that have the same token ID as pseudoOutputAmount,
+                they are likely change. There should be at most one. But these amounts may have been
+                added as regular outputs. This doesn't really make sense, but valid SignedContingentInputs
+                can be built like this.
+                */
+                changeAmount = changeAmount.add(amount);
+            }
+            else {
+                //TODO: on API level 24, we can use getOrDefault to simplify the logic here
+                Amount existingAmount = totalOutlays.get(amount.getTokenId());
+                if(null == existingAmount) existingAmount = new Amount(BigInteger.ZERO, amount.getTokenId());
+                totalOutlays.put(amount.getTokenId(), existingAmount.add(amount));
+            }
+        }
+
+        /*
+        This can happen if the party building a SignedContingentInput requests more than their entire
+        payment as a required output. In such a case, the consumer of the SCI only spends tokens and does
+        not receive anything.
+         */
+        if(changeAmount.compareTo(pseudoOutputAmount) > 0) {
+            totalOutlays.put(changeAmount.getTokenId(), changeAmount);
+        }
+
         return totalOutlays;
+
     }
 
     @NonNull
