e0b77f3b22afa3a354c525a479b52b7ede711a05
Eran Rundstein <eran@rundste.in>
2025-03-17 15:05:15 -0700

    Get CircleCI to pass (#145)

--- docker/scripts/run_connected_tests.sh
+++ docker/scripts/run_connected_tests.sh
@@ -2,14 +2,15 @@
 gradle :testApp:assembleDebug
 gradle :android-sdk:assembleGrpcDebugAndroidTest
 
-gcloud config set project mobilecoin-60c
+gcloud config set project android-sdk-exper
 gcloud auth activate-service-account \
-    ci-service-account@mobilecoin-60c.iam.gserviceaccount.com \
-    --key-file=/home/gradle/service-key.json --project=mobilecoin-60c
+    circleci@android-sdk-exper.iam.gserviceaccount.com \
+    --key-file=/home/gradle/service-key.json --project=android-sdk-exper
+
 
 gcloud firebase test android run \
     --type instrumentation \
-    --device model=Nexus5X,version=24 \
+    --device model=Pixel2.arm,version=33 \
     --app testApp/build/outputs/apk/debug/testApp-debug.apk \
     --test android-sdk/build/outputs/apk/androidTest/grpc/debug/android-sdk-grpc-debug-androidTest.apk \
     --timeout 45m
